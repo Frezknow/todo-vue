7c7017b9e79471bcf6184db93078b7169ed77566
frezknow <frezknow@gmail.com>
2019-03-31 00:05:06 -0400

    Components and child components for Vue application

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -2,32 +2,38 @@
   <div>
    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo"/>
    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
-    <todo-item v-for="(todo , index) in todosFiltered" :key="todo.id" :todo="todo"  :index="index" :checkAll="!anyRemaining" @removedTodo="removeTodo(index)" @finishedEdit="finishedEdit">
+    <todo-item v-for="(todo , index) in todosFiltered" :key="todo.id" :todo="todo"  :index="index" :checkAll="!anyRemaining">
     </todo-item>
    </transition-group>
    <div class="extra-container">
-     <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"/>Check All</label></div>
-     <div>{{remaining}} items left</div>
+     <todo-check-all :anyremaining="anyremaining"></todo-check-all>
+     <todo-items-remaining :remaining="remaining"></todo-items-remaining>
    </div>
    <div class="extra-container">
+     <todo-filtered></todo-filtered>
      <div>
-      <button :class="{active:filter =='all'}" @click="filter='all'">All</button>
-      <button :class="{active:filter =='active'}" @click="filter='active'">Active</button>
-      <button :class="{active:filter =='completed'}" @click="filter='completed'">Completed</button>
-     </div>
-     <div>
-       <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
-     </div>
+      <transition name="fade">
+       <todo-clear-completed :showClearCompletedButton="showClearCompletedButton"></todo-clear-completed>
+      </transition>
+    </div>
    </div>
   </div>
 </template>
 
 <script>
 import TodoItem from './TodoItem';
+import TodoItemsRemaining from './TodoItemsRemaining';
+import TodoCheckAll from './TodoCheckAll';
+import TodoFiltered from './TodoFiltered';
+import TodoClearCompleted from './TodoClearCompleted';
 export default {
   name: 'TodoComponent',
   components:{
     TodoItem,
+    TodoItemsRemaining,
+    TodoCheckAll,
+    TodoFiltered,
+    TodoClearCompleted,
   },
   data () {
     return {
@@ -35,8 +41,8 @@ export default {
      idForTodo:3,
      editing:false,
      completed:false,
-     beforeEditCache:'',
      filter:'all',
+     beforeEditCache:'',
      todos:[
        {
        'id':1,
@@ -105,6 +111,20 @@ export default {
      clearCompleted(){
         this.todos = this.todos.filter(todo=> !todo.completed);
      }
+  },
+  created(){
+    eventBus.$on('removedTodo',(index)=>this.removeTodo(index));
+    eventBus.$on('finishedEdit',(data)=>this.finishedEdit(data));
+    eventBus.$on('checkAllChanged',(checked)=> this.checkAllTodos(checked));
+    eventBus.$on('filterChanged',(filter)=>this.filter=filter);
+    eventBus.$on('clearCompletedTodos',()=>this.clearCompleted());
+  },
+  beforeDestroy(){
+    eventBus.$off('removedTodo',(index)=>this.removeTodo(index));
+    eventBus.$off('finishedEdit',(data)=>this.finishedEdit(data));
+    eventBus.$off('checkAllChanged',(checked)=> this.checkAllTodos(checked));
+    eventBus.$off('filterChanged',(filter)=>this.filter=filter);
+    eventBus.$off('clearCompletedTodos',()=>this.clearCompleted());
   }
 }
 </script>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -5,8 +5,11 @@
    <div v-if="!editing" @dblclick="editTodo" class="todo-item-label" :class="{completed:completed}">{{title}}</div>
    <input v-else class="todo-item-edit" type="text" v-model="title" @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus/>
   </div>
-  <div class="remove-item" @click="removeTodo(index)">
+  <div>
+    <button @click="pluralize">Plural</button>
+   <span class="remove-item" @click="removeTodo(index)">
     &times;
+   </span>
   </div>
  </div>
 </template>
@@ -51,7 +54,7 @@
    },
    methods:{
      removeTodo(index){
-       this.$emit('removedTodo',index)
+       eventBus.$emit('removedTodo',index)
      },
      editTodo(){
        this.beforeEditCache = this.title
@@ -62,7 +65,7 @@
          this.title = this.beforeEditCache
        }
        this.editing = false
-       this.$emit('finishedEdit',{
+       eventBus.$emit('finishedEdit',{
          'index':this.index,
          'todo':{
            'id':this.id,
@@ -77,6 +80,27 @@
        this.editing = false
 
      },
+     pluralize(){
+       eventBus.$emit('pluralize');
+     },
+     handlePluralize(){
+       this.title = this.title + "s"
+     }
+   },
+   created(){
+     eventBus.$on('pluralize',this.handlePluralize)
+     eventBus.$emit('finishedEdit',{
+       'index':this.index,
+       'todo':{
+         'id':this.id,
+         'title':this.title,
+         'completed':this.completed,
+         'editing':this.editing,
+       }
+     })
+   },
+   beforeDestroy(){
+     eventBus.$off('pluralize',this.handlePluralize)
    }
   }
 </script>
